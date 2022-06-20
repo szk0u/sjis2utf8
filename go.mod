9e7d21b45268414ce2fb6464932b88ba4c51e587
szk0u <szk0u3@gmail.com>
2022-06-21 00:09:26 +0900

    sjisへの変換処理を追加

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/szk0u/utf8tosjis
 
 go 1.18
+
+require golang.org/x/text v0.3.7
--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+	"io"
 	"os"
+	"path/filepath"
+	"strings"
+	"unicode/utf8"
 )
 
 func main() {
@@ -13,25 +20,99 @@ func main() {
 	}
 
 	for _, v := range os.Args[1:] {
-		err := handleFilepath(v)
+		// ファイルの存在チェック
+		_, err := os.Stat(v)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%sは存在しません\n", v)
+			os.Exit(1)
+			return
+		}
+
+		err = handleFilepath(v)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%sの変換に失敗しました。%v\n", v, err)
 			os.Exit(1)
 			return
 		}
 	}
 }
 
-func handleFilepath(filepath string) error {
-	// ファイルの存在チェック
-	_, err := os.Stat(filepath)
+func handleFilepath(path string) error {
+	// 元のファイル名を元にファイルを作成
+	absPath, err := filepath.Abs(path)
 	if err != nil {
 		return err
 	}
 
-	// 元のファイル名を元にファイルを作成
+	ext := filepath.Ext(path)
 
-	// utf8からsjisに変換
+	pathWithoutExtension := strings.TrimSuffix(absPath, ext)
+
+	newPath := pathWithoutExtension + "_converted" + ext
+
+	existingFile, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer existingFile.Close()
+
+	newFile, err := os.Create(newPath)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+
+	writer := &runeWriter{transform.NewWriter(newFile, japanese.ShiftJIS.NewEncoder())}
+	tee := io.TeeReader(existingFile, writer)
+	s := bufio.NewScanner(tee)
+	for s.Scan() {
+	}
 
 	return nil
+}
+
+type runeWriter struct {
+	w io.Writer
+}
+
+func (rw *runeWriter) Write(b []byte) (int, error) {
+
+	var err error
+
+	l := 0
+
+loop:
+
+	for len(b) > 0 {
+
+		_, n := utf8.DecodeRune(b)
+
+		if n == 0 {
+
+			break loop
+
+		}
+
+		_, err = rw.w.Write(b[:n])
+
+		if err != nil {
+
+			_, err = rw.w.Write([]byte{'?'})
+
+			if err != nil {
+
+				break loop
+
+			}
+
+		}
+
+		l += n
+
+		b = b[n:]
+
+	}
+
+	return l, err
+
 }
